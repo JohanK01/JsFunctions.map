8e98b750e22f034d4066df0d21c595ae104486cd
JohanK <klausenjohan23@gmail.com>
2022-01-13 15:01:10 +0100

    label value pushed to array then gets sorted by unique

--- one.js
+++ one.js
@@ -3,4 +3,23 @@
     let header = document.createElement('h2');
     header.textContent = 'ONE';
     document.getElementById('content').appendChild(header);
-   
+    let cars = [];
+    let uniqueArr = [];
+    
+function addData() {
+    
+    function map(f,a){
+    let result = []; // Create a new Array
+    let i; // Declare variable
+
+    for (i = 0; i != a.length; i++)
+      result[i] = f(a[i]);
+    return result; 
+    }
+    
+    let carMake = document.getElementById('make').value;
+    cars.push(carMake);
+    console.log(cars);
+    uniqueArr = [...new Set(cars.map((item) => item))];
+    console.log(uniqueArr)
+}
